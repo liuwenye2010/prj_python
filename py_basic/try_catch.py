8d29bf0aedcb34b6ab091b7a7af68fe8955143cc
johx <johndoe@example.com>
2020-09-24 23:24:16 +0800

    Signed-off-by: johx <johndoe@example.com>

--- py_basic/try_catch.py
+++ py_basic/try_catch.py
@@ -2,21 +2,22 @@ import os
 import re
 import sys
 import time
-import serial
-#import serial.tools.list_ports
+import serial #pip install serail 
+import serial.tools.list_ports # pip install pyserial 
 import errno
 import time
 import hashlib
 
-# port_list = list(serial.tools.list_ports.comports())
-# if(len(port_list) <= 0):
-# 	print("No serial port connected found")
-# 	sys.exit(0)
-# else:
-# 	ls = len(port_list)
-# 	for i in range(ls):
-# 		print(list(port_list[i])[0], list(port_list[i])[1])
-# sys.exit(0)
+port_list = list(serial.tools.list_ports.comports())
+if(len(port_list) <= 0):
+	print("no serial port connected found")
+	sys.exit(0)
+else:
+	ls = len(port_list)
+	print("UART PORT LIST:")
+	for i in range(ls):
+		print(list(port_list[i])[0], list(port_list[i])[1])
+
 
 try: 
 	while True:
@@ -25,3 +26,5 @@ try:
 		pass
 except KeyboardInterrupt:
     print("SIGINT: Key Interrupt")
+
+sys.exit(0)
