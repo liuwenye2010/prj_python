bae70d0fa7c73b36d15578598535331470cee275
johx <johndoe@example.com>
2020-10-17 15:39:41 +0800

    Signed-off-by: johx <johndoe@example.com>

--- csv2wave/csv2wave.py
+++ csv2wave/csv2wave.py
@@ -9,6 +9,10 @@ from pprint import pprint
 import os
 import glob 
 import argparse
+import os.path
+import sys
+
+
 
 default_file_to_convert = "audio.csv"
 column_name_to_parse    = "MOSI"
@@ -16,7 +20,7 @@ column_name_to_parse    = "MOSI"
 def cmd_parser():
     global column_name_to_parse 
     global default_file_to_convert
-    parser = argparse.ArgumentParser(description="This program Load & convert data from CSV file and convert into Mono Wave file")
+    parser = argparse.ArgumentParser(description="This program load one column data from CSV file and convert into one mono wave file")
     parser.add_argument("-i", dest="input" ,required=False, help="input file name (e.g audio.csv)")
     parser.add_argument("-c", dest="column_name" ,required=False, help="column name to parse (e.g MOSI)")
     parser.add_argument("-s", dest="sample_rate" ,required=False, help="sample rate for wave (e.g 16000)")
@@ -56,7 +60,7 @@ def cmd_parser():
         if sample_width  not in ['0', '1','2', '4'] :
             print("ERROR: Invalid sample_width parameter value")
             parser.print_help()
-            exit(1)
+            sys.exit(-1)
         else:
             sample_width = int(sample_width)
             print("sample_width:{0}".format(sample_width))
@@ -68,8 +72,20 @@ def cmd_parser():
     print("[INFO] sample_rate          is {0}".format(sample_rate))
     output_file_name = input_file_name + '.wav'
     print("[INFO] input is {0}, output is {1}".format(input_file_name,output_file_name))
-    csv2wave_mono(sample_rate,sample_width,input_file_name, output_file_name) # 16KHz/16bits
-    print("done")
+    if os.path.isfile(input_file_name):
+        try:
+            csv2wave_mono(sample_rate,sample_width,input_file_name, output_file_name) # 16KHz/16bits
+        except KeyboardInterrupt:
+            print("[INFO] User Key Interrupt exit")
+            sys.exit(-1)
+        else:
+            pass
+    else:
+        print("\n[ERROR]:input file {0} is not exist !\n".format(input_file_name))
+        parser.print_help()
+        sys.exit(-1)
+
+    print("Done")
 
 
 def parse_timestamp(text):
@@ -103,5 +119,4 @@ def csv2wave_mono(sampleRate,sampleWidth,input_file, output_file):
     wavef.close()
 
 if __name__ == "__main__":
-    #csv2wave_mono(16000,2,"sound_audio_spi.csv","sound_audio_spi.csv.wav") # 16KHz/16bits
     cmd_parser()
