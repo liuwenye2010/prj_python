03f86c3385a50cd90d8c3db48f41d4bf298f8079
johx <johndoe@example.com>
2021-08-04 16:46:51 +0800

    update the sequence

--- py_andorid_autotest/android_autotest.py
+++ py_andorid_autotest/android_autotest.py
@@ -5,11 +5,13 @@ import sys
 import subprocess
 import re
 from time import sleep
+import unittest
 #import uiautomator as ui 
 #import uiautomator2 as ui2
 #import pysox
 from lxml import etree
 #from xml.etree.ElementTree import ElementTree
+
 
 
 def exec_adb(cmd): 
@@ -65,13 +67,68 @@ def input_text(text):
     exec_adb('shell input text %s' % text)
 
 def on_keyevent(keyevent):
-    exec_adb('shell keyevent %s' % keyevent)
+    exec_adb('shell input keyevent %s' % keyevent)
 
 def start_activity(text):
     exec_adb('shell am  start -n %s' % text)
 
 def stop_activity(text):
     exec_adb('shell am  force-stop %s' % text)
+
+def on_key_home():
+    on_keyevent(3)
+
+def on_key_back():
+    on_keyevent(4)
+
+def on_key_power():
+    on_keyevent(26)
+
+def on_key_unlock():
+    #input touchscreen swipe 930 880 930 380 #Swipe UP
+    on_keyevent(82)
+
+def is_screen_on():
+    ret = exec_adb("shell dumpsys power | grep 'mHoldingDisplaySuspendBlocker' | grep -oE '(false|true)'")
+    if "true" in str(ret) :
+        print("INFO:screen ON")
+        return 1
+    else:
+        print("INFO:screen OFF")
+        return 0 
+
+def is_screen_lock():
+    ret = exec_adb("shell dumpsys power | grep 'mHoldingWakeLockSuspendBlocker' | grep -oE '(false|true)'")
+    if "true" in str(ret) :
+        print("INFO:screen LOCKED")
+        return 1
+    else:
+        print("INFO:screen UNLOCED")
+        return 0 
+
+    
+def screen_on():
+    ret = exec_adb("shell dumpsys power | grep 'Display Power' | grep -oE '(ON|OFF)'")
+    if "OFF" in str(ret) :
+        on_key_power()
+    else:
+        pass
+def screen_off():
+    ret = exec_adb("shell dumpsys power | grep 'Display Power' | grep -oE '(ON|OFF)'")
+    if "ON" in str(ret) :
+        on_key_power()
+    else:
+        pass
+def capture_dmesg():
+    exec_adb('root')
+    exec_adb('remount')
+    exec_adb('push ./codec_parse.sed /data')
+    subprocess.Popen('adb shell cat /dev/kmsg  | grep as339 | sed -f /data/codec_parse.sed > /data/dmesg_log.txt ')
+
+
+
+def get_dmesg_log():
+    exec_adb('pull /data/dmesg_log.txt .')
 
 def page_on_click(text,t_type='text'):
     dump_layout()
@@ -81,93 +138,84 @@ def page_on_click(text,t_type='text'):
         a,b,c,d = map(int,ret)
         print('a={0},b={1},c={2},d={3}'.format(a,b,c,d))
         on_click(a,b)
+        return  1  
     else:
         print("WARN:can not find {0}".format(text))
+        return  0
 
 def page_check_value(text, t_type, attr):
     dump_layout()
     return read_value(text,t_type,attr)
 
 def voice_match_stress(debug):
+    exec_adb('root')
+    exec_adb('remount')
+    if is_screen_on() == 0:
+        screen_on()
+    if is_screen_lock() == 0: 
+        on_key_unlock()
+    ret_vm  = 0 
     if debug == 0 :
-        exec_adb('root')
-        exec_adb('remount')
-
-        #C:\Users\johl\Desktop>adb shell  dumpsys  activity | findstr  "mFocused"
-        #  mFocusedApp=ActivityRecord{f526d53 u0 com.google.android.googlequicksearchbox/com.google.android.apps.gsa.velvet.ui.settings.SettingsActivity t60}
-        #    mFocusedWindow=Window{1ceb8f9 u0 com.google.android.googlequicksearchbox/com.google.android.apps.gsa.velvet.ui.settings.SettingsActivity}
-        #adb shell am  start -n  com.google.android.googlequicksearchbox/com.google.android.apps.gsa.velvet.ui.settings.SettingsActivity
-        #stop_activity('com.google.android.googlequicksearchbox/com.google.android.apps.gsa.velvet.ui.settings.SettingsActivity')
-        
-        page_on_click('More')
-        page_on_click('Settings')
+        on_key_home()
+        #on_key_back()
+        #capture_dmesg()
         start_activity('com.google.android.googlequicksearchbox/com.google.android.apps.gsa.velvet.ui.settings.SettingsActivity')
         page_on_click('Voice')
         page_on_click('Voice Match')
         page_on_click('No thanks')
         page_on_click('Hey Google')
         ret = page_check_value('com.google.android.googlequicksearchbox:id/itemview_control_switch','resource-id','checked')
-        if ret == 'true': 
-            page_on_click('Next')
+        if page_on_click('Next')  > 0:
             page_on_click('I agree')
-            page_on_click('Continue')
+            ret = 'true'
+        if ret == 'true': 
+            print("INFO:Voice Match ON")
+            #page_on_click('Next')
+            #page_on_click('I agree')
+            #page_on_click('Continue')
             page_on_click('Voice model')
             page_on_click('Retrain voice model')
             for i in range(4):
                 exec_sox_plbk('./ok_google_one_trigger_short.wav')
                 sleep(2)
-            page_on_click('Finish')
-            page_on_click('Cancel')
+            ret_vm =  page_on_click('Finish')
+            if ret_vm == 0:
+                get_dmesg_log()
+                return  -1 
         else:
+            print("INFO:Voice Match OFF")
             sleep(3)
+            page_on_click('Next')
+            page_on_click('I agree')
+            page_on_click('Cancel')
             page_on_click('OK')
-        
-        #back 
-        page_on_click('Navigate up','content-desc')
-
-        if 0 :
-            pattern = re.compile(r'\[(\d+),(\d+)\]\[(\d+),(\d+)\]')
-            ret = pattern.match('[0,598][2557,635]')
-            print(ret)
-            print(ret.groups())
-            print(ret.group(0))
-            print(ret.group(1))
-            print(ret.group(2))
-            print(ret.group(3))
-            print(ret.group(4))
-            print(ret.string)
-            print(map(int,ret.groups()))
-
+            #on_key_back()
+        #page_on_click('Navigate up','content-desc')
         #stop_activity('com.google.android.googlequicksearchbox/com.google.android.apps.gsa.velvet.ui.settings.SettingsActivity')
-
-        #adb shell getevent  -p | grep -e "0035" -e "0036
-        # $ adb shell getevent  -p | grep -e "0035" -e "0036"
-        #                 0035  : value 0, min 0, max 1600, fuzz 0, flat 0, resolution 0
-        #                 0036  : value 0, min 0, max 2560, fuzz 0, flat 0, resolution 0
-        #                 0035  : value 0, min 0, max 2644, fuzz 0, flat 0, resolution 31
-        #                 0036  : value 0, min 0, max 1440, fuzz 0, flat 0, resolution 31
-        #                 0031  0032  0033  0034  0035  0036  0037  0038
-
-        #adb shell getevent  | grep -e "0035" -e "0036"
+        #get_dmesg_log()
         print("doneI")
+        return 0
     else: 
-        #x = subprocess.check_output("adb devices")
-        #print(x)
         dump_layout()
-        page_check_value('com.google.android.googlequicksearchbox:id/itemview_control_switch','resource-id','checked')
-
+        ##ERR: start_activity('com.google.android.googlequicksearchbox/com.google.android.apps.search.googleapp.activity.GoogleAppActivity')
+        start_activity('com.google.android.googlequicksearchbox/com.google.android.apps.gsa.velvet.ui.settings.SettingsActivity')
+        #page_check_value('com.google.android.googlequicksearchbox:id/itemview_control_switch','resource-id','checked')
         print("doneE")
+    on_key_home()
 
 if __name__ == "__main__":
     debug = 0
     loop_cnt = 1
-    if len(sys.argv) > 1 :
-        debug = int(sys.argv[1])
+    if len(sys.argv) > 1:
+        loop_cnt = int(sys.argv[1])
     if len(sys.argv) > 2 :
-        loop_cnt = int(sys.argv[2])
+        debug = int(sys.argv[2])
     if debug == 0 :
         #print(loop_cnt)
         for i in range(loop_cnt):
-            voice_match_stress(debug)
+            vs_ret = voice_match_stress(debug)
+            if vs_ret < 0:
+                print("FAIL: VOICE_MATCH_TEST FAIL")
+                break
     else:
        voice_match_stress(debug)
