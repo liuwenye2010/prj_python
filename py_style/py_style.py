6c1c73df09c455dd709bc9c539bb8ffa68f78960
johx <johndoe@example.com>
2021-08-19 14:29:47 +0800

    [0000]py-style  (pep8/google) : pep8, autopep8, yapf, pylint

---
+++ py_style/py_style.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""A one line summary of the module or program, terminated by a period.
+"""
+import os
+#autopep8/ yapf / pep8 /pylint /
+#> yapf --style google -i test.py
+#> autopep8  -i test.py 
+
+names = ['ads', 'hps', 'johl']
+
+# TODO(johl@synaptics.com): Use a "*" here for string repetition.
+x = ("hdddddd" "hhhh")
+if __name__ == "__main__":
+    assert (1 == 2, "this should fail")
+    print(x)
+    pass
